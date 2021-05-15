7729d3234282d6d99a2a72a8660210e8b5981ca6
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2021-05-15 01:46:55 +0100

    Rough draft of installkeygenerator.sh
    Downloads, extracts and install requirements for nodejs to run app.js

---
+++ installkeygenerator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Args incoming
+username=$1
+path=/home/$username
+filepath=$path/exchange-fullnode-coti.rar
+wget -O $filepath https://coti.tips/downloads/exchange-fullnode-coti.rar
+
+sudo apt install npm
+sudo apt install unrar
+
+unrar x -o+ $filepath $path
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+cd $path/exchange-fullnode
+npm install
+rm $filepath
