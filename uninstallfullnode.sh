e17de8ab1dece6fe159b45a5876626b056e22823
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2021-05-29 16:30:26 +0100

    Update uninstallfullnode.sh

--- uninstallfullnode.sh
+++ uninstallfullnode.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 read -p "What is your ubuntu username (its likely to be coti if you followed the default suggestion on the install) ?: " username
-rm -R /home/$username/coti-fullnode
-systemctl stop cnode.service
-systemctl disable cnode.service
+rm -R /home/$username/coti-fullnode 2> /dev/null
+rm -R /home/$username/coti-node 2> /dev/null
+systemctl stop cnode.service 2> /dev/null
+systemctl disable cnode.service 2> /dev/null
 rm /etc/systemd/system/cnode.service 2> /dev/null
 rm /usr/lib/systemd/system/cnode.service 2> /dev/null
 systemctl daemon-reload
 systemctl reset-failed
+echo "Uninstall Completed"
